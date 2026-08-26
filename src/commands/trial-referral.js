24953ce06cc2038962ce6239762c100b2ee5237e
Kgaming <147094790+Kgamingytch@users.noreply.github.com>
2026-08-26 07:18:27 +0200

    Update trial-referral.js

--- src/commands/trial-referral.js
+++ src/commands/trial-referral.js
@@ -1,14 +1,14 @@
 const { SlashCommandBuilder, MessageFlags } = require("discord.js");
 
-module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("trial-referral")
-    .setDescription("Refer a trial trainer (coming soon)"),
-
-  async execute(interaction) {
-    return interaction.reply({
-      content: "**/trial-referral** is unavailable at the moment. Explanations will be shared soon.",
-      flags: MessageFlags.Ephemeral,
-    });
-  },
-};
+# module.exports = {
+#  data: new SlashCommandBuilder()
+#    .setName("trial-referral")
+#    .setDescription("Refer a trial trainer (coming soon)"),
+#
+#  async execute(interaction) {
+#    return interaction.reply({
+#      content: "**/trial-referral** is unavailable at the moment. Explanations will be shared soon.",
+#      flags: MessageFlags.Ephemeral,
+#    });
+#  },
+# };
